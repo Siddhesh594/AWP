df4d9454d3635e16b9e51de91ad174a816f7dcf0
Siddhesh594 <siddhesh.patel23@pcu.edu.in>
2025-11-19 05:59:35 +0530

    Add Mongoose schema for Message model

--- models/Message.js
+++ models/Message.js
@@ -1,1 +1,9 @@
 
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+    userMessage: { type: String, required: true },
+    botReply: { type: String, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Message", messageSchema);
